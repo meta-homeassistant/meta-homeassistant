25670f665d087aad9e7a4cd172e64eb3075ce4a8
Tom Geelen <t.f.g.geelen@gmail.com>
2024-09-01 21:45:03 +0000

    python3-matter-server 6.2.2 -> 6.3.0
    update parse-requirements.py

--- scripts/parse_requirement.py
+++ scripts/parse_requirement.py
@@ -139,6 +139,7 @@ def parse_manifests(ha_path, upgrade_only, integrations_only):
 
                 integrations = [
                     "acer_projector",
+                    "accuweather",
                     "airthings_ble",
                     "amazon_polly",
                     "aruba",
